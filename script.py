b46a38c9194fb87b02222601dacb83803a1b83e6
edely <edely.gomes@gmail.com>
2018-07-09 15:09:10 -0300

    adicionando pasta para discursos

--- script.py
+++ script.py
@@ -17,6 +17,7 @@ def choose_deputy(DEP):
     '''
     Esta função seleciona o deputado de acordo com seu código e acessa sua página ed pronunciametos
     '''    
+    print('choose_deputy')
     dep_page = requests.get(BASE_URL+DEP).text
     dep_soup = BS(dep_page, PARSER)
     dep_name = dep_soup.find('h1', class_='titulo-pagina').text.split('em')[0]
@@ -28,6 +29,7 @@ def choose_deputy(DEP):
     iterate_deputy_years(min_y, max_y)
 
 def iterate_deputy_years(min_y, max_y):
+    print('iterate_deputy_years')
     for year in range(int(min_y), int(max_y)+1):
         year_url = BASE_URL+DEP+'/'+str(year)
         year_page = requests.get(year_url)
@@ -43,9 +45,9 @@ def iterate_deputy_years(min_y, max_y):
         max_page = range_page[-1]
         for page in range(int(min_page), int(max_page)+1):             
             get_page(page, year_url)
-            sys.exit()
 
 def get_page(page, year_url):
+    print('get_page')
     page_html =  requests.get(year_url+'/'+str(page)).text
     page_speach_trs = BS(page_html, PARSER).select('.portlet-body table a')
     for i in page_speach_trs:
@@ -56,16 +58,14 @@ def get_page(page, year_url):
         name = speach.select('.well dd:nth-of-type(1) span:nth-of-type(1)')[0].text
         date = speach.select('.well dd:nth-of-type(2)')[0].text
         integral_text = speach.select('.texto-integral')[0].text
-
         save_page(integral_text, date, name)
-        sys.exit()
-
 
 def save_page(integral_text, date, name):
-    if not os.path.exists(slugify(name)):
-        os.makedirs(slugify(name))
-
-    f = open(slugify(name)+'/'+slugify(date)+'.txt', "w")
+    print('save_page')
+    if not os.path.exists('speachs/'+slugify(name)):
+        os.makedirs('speachs/'+slugify(name))
+    print('Saving speach from {}'.format(date))
+    f = open('speachs/'+slugify(name)+'/'+slugify(date)+'.txt', "w")
     f.write(integral_text.replace('             ', ''))
     f.close()
 
